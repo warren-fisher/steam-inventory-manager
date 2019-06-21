efe26dc8b15dd3c26aa1cb6a3fd1e8134fb71542
Warren Fisher <github_fisher@wfisher.info>
2019-06-21 11:27:54 -0300

    Restructure market_api into a class

--- market_api.py
+++ market_api.py
@@ -4,36 +4,41 @@ import requests
 import re
 from ast import literal_eval
 
-try: 
-    response = requests.get('https://steamcommunity.com/market/listings/730/Prisma%20Case')
+# TODO: Currently pricing info is always in USD$
+# The following link is an example of how the currency can be set to rubles,
+# however, I am not sure if setting language to russian is neccessary
+# https://steamcommunity.com/market/listings/730/Prisma%20Case?l=russian&cc=ru
 
-    response.raise_for_status()
-
-except HTTPError as http_err:
-    print(f"An HTTP error occured: {http_err}")
-
-except Exception as err:
-    print(f"Other error occured: {err}")
+class MarketListing:
     
-soup = bs.BeautifulSoup(response.content, 'lxml')
+    def __init__(self, item_name):
+        self.name = item_name
+        self.url = f'https://steamcommunity.com/market/listings/730/{self.name}'
 
-for script in soup.find_all('script'):
-    print(script.text)
+        self.parse()
 
-with open("./html.html", mode='wt', encoding='utf-8') as file:
-    file.write(soup.prettify())
+    def get_response(self):
+        response = requests.get(self.url)
+        response.raise_for_status()
+        return response
 
-script_info = ''
-for script in soup.find_all('script'):
-    if 'var line1' in script.text:
-        script_info = script.text
+    def parse(self):
+        response = self.get_response()
+        soup = bs.BeautifulSoup(response.content, 'lxml')
 
-search = re.search('var line1(.+);', script_info)
-pricing = literal_eval(search.group(1)[1:])
+        for script in soup.find_all('script'):
+            if 'var line1' in script.text:
+                text = script.text
 
-for pricing_window in pricing: 
-    date = pricing_window[0]
-    price = pricing_window[1]
-    volume = pricing_window[2]
+        search = re.search('var line1(.+);', text)
+        string_representation_of_list = search.group(1)[1:]
 
-    print(f"{date} : {price} : {volume}")
+        self.pricing_history = literal_eval(string_representation_of_list)
+
+    def price(self):
+        price_now_index = len(self.pricing_history) - 1
+        return self.pricing_history[price_now_index][1]
+
+if __name__ == "__main__":
+    m = MarketListing('Prisma Case')
+    print(m.price())
